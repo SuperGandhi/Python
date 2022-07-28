f0298cec38fd706d814e0069ce160fbf6a18ceef
SuperGandhi <patrickborjabueno@hotmail.com>
2022-07-28 17:02:09 -0500

    Complete

--- 1-Control-flujo/main.py
+++ 1-Control-flujo/main.py
@@ -84,8 +84,54 @@ tupla = (1,2,3,4,5,6,7)
 # for current_value in tupla:
 #     print(current_value)
 
-for numero in range(2,7):
-    print(numero)
+# for numero in range(2,7):
+#     print(numero)
     
-listillo = len(lista)
-print('The list have' ,listillo , 'items')
+# listillo = len(lista)
+# print('The list have' ,listillo , 'items')
+
+# lista = ['hi', 'bye', 'message']
+
+# if 'message' not in 'lista':
+#     print('Yes I find the word')
+
+lista = [4,5,6,1,10,12,3]
+list_alphabet = ['A','b','Z','y','S','q','W','r']
+print(lista)
+
+list_order = sorted(lista)
+list_order = sorted(lista, reverse=True)
+list_alphabet = sorted(list_alphabet)
+print(list_order)
+print(list_alphabet)
+
+# la palabra reservada math actua como un "switch" está se encuentra
+# disponible a partir de la versión 3.10 de python 
+
+# value = 10
+
+# math value:
+#         case 1:
+#             print('Value is one')
+#         case 2:
+#             print('Value is two')
+#         case 3:
+#             print('Value is three')
+#         case 4:
+#             print('Value is four')
+#         case 5:
+#             print('Value is five')
+            
+list = ['hi', 'bye', 'message']
+
+for word in lista:
+    # para dejar declarada una función o bucle, esto también para que no nos tire un error al tener 
+    # un espacio en blanco o vacio
+    pass 
+    # las variables solo se pueden usar dentro de un bucle y su hijo 
+    temporal = False
+    if word == "message":
+        print('Find the word message')
+        break
+
+print(list)

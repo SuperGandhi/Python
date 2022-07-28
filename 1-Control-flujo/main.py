8bad119103b7aab3baa803e0aa7981fbe4fdb2fd
SuperGandhi <patrickborjabueno@hotmail.com>
2022-07-28 13:29:56 -0500

    Complete

--- 1-Control-flujo/main.py
+++ 1-Control-flujo/main.py
@@ -48,3 +48,44 @@ print(result)
 
 result = a < 5 ^ c < 7;
 print(result)
+
+
+a = 10
+b = 20
+c = 30
+
+# a > b
+# a < b and b < a or a > b and b > a
+# SOlo se ejecutun las condiciones cuando son verdaderas
+# if a >= 10 and c >= 30:
+#     print('Diez es mayor igual y treinta es mayor igual')
+# elif b >= 20:
+#     print("A es mayor o igual que 6")
+# else:
+#     print('NO SE CUMPLE NINGUNA CONDICIÓN')
+# print('F')
+
+contador = 0
+"""
+        What's is the difference between break 
+        and continue:
+        Continue jump of the next iteration 
+"""
+while contador <= 10:
+        print('Contador vale: ', contador)
+        contador += 1
+        continue
+
+print('END WHILE')
+
+lista = [1,2,3,4,5,6,7]
+tupla = (1,2,3,4,5,6,7)
+
+# for current_value in tupla:
+#     print(current_value)
+
+for numero in range(2,7):
+    print(numero)
+    
+listillo = len(lista)
+print('The list have' ,listillo , 'items')

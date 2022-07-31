452fe6fcf900be38f58e89a1c849fd73b627d9ad
SuperGandhi <patrickborjabueno@hotmail.com>
2022-07-31 15:01:17 -0500

    Complete

--- 2-Funciones/Exercises/number_prime.py
+++ 2-Funciones/Exercises/number_prime.py
@@ -1,4 +1,8 @@
-def number_prime(n):
-    if (n / 1 and n / n ):
-        pass
-        
+
+number_prime = lambda n: all([n % i for i in range (2,n)])
+
+for i in range(2,100):
+    n_prime = number_prime(i)
+    if n_prime:
+        print(i)
+    

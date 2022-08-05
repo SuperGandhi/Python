39ef3df3508fd8172fb6c42e5d74b83f21a89bc9
SuperGandhi <patrickborjabueno@hotmail.com>
2022-08-04 22:46:39 -0500

    Correct

--- 3-Clases-Objetos/main.py
+++ 3-Clases-Objetos/main.py
@@ -1,14 +1,17 @@
-class Car:
+
+# clase base
+class Juguete:
     """
         cuando  una propiedad o funcion empiezan con guion bajo no se debe manipular 
         fuera de la clase(convención)
     """
+    # variable de clase
     _encendido = False
     
     def enciende(self):
         # variable shadowing dos variables del mismo nombres que no están relacioandas
         self._encendido = True
-        pass
+
     
     def apaga(self):
         self._encendido = False
@@ -16,9 +19,32 @@ class Car:
     def isEncendido(self):
         return self._encendido
 
-a1 = Car()
-a1.apaga()
-print(a._encendido)
+        """
+            Herencia:
+            Consiste en que una clase hereda 
+            las propiedades y métodos de otra clase 
+            
+            También existen las herencias múltiples
+            pero se considera una mala práctica hay que
+            evitarla en la medida de lo posible
+            
+        """
 
-d2 = Car()
-d2.encendido()
+class Dino(Juguete):
+    color = None
+    name = None
+    
+    def _init_(self, name):
+        self.color = "Green"
+        self.name = name
+        print("Estoy en el constructor", name)
+    
+    def verEscamas(self):
+        pass
+
+p = Dino()
+print(p.color)
+print(p.name)
+
+
+

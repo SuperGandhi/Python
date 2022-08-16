aaf513423807c715ec2f71c99cdee2dc4e58dfa0
SuperGandhi <patrickborjabueno@hotmail.com>
2022-08-15 23:41:46 -0500

    Complete

--- 7-GUI/main.py
+++ 7-GUI/main.py
@@ -20,20 +20,27 @@ window.columnconfigure(1,weight=3)
 
 label = ttk.Label
 
-
+# Etiqueta usuario
 username_label =ttk.Label(window, text='Username: ')
 username_label.grid(column=0, row=0, sticky=tkinter.W, padx=5, pady=5)
 
-
+# Campo usuario
 username_entry = ttk.Entry(window)
 username_entry.grid(column=1, row=0, sticky=tkinter.E, padx=5, pady=5) 
 
 
-
+# Etiqueta password
 password_label =ttk.Label(window, text='Password: ')
-password_label.grid(column=0, row=0, sticky=tkinter.W, padx=5, pady=5)
-password_entry = ttk.Entry(window)
-password_entry.grid(column=1, row=0, sticky=tkinter.E, padx=5, pady=5)
+password_label.grid(column=0, row=1, sticky=tkinter.W, padx=5, pady=5)
+
+# Campo password
+password_entry = ttk.Entry(window, show='-')
+password_entry.grid(column=1, row=1, sticky=tkinter.E, padx=5, pady=5)
+
+
+#Boton
+login_button = ttk.Button(window)
+login_button.grid(column=1, row=3, sticky=tkinter.E, padx=5, pady=5)
 
 window.mainloop()
 
